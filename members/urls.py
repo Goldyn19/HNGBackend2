1c629140e22285dfc46f167f5fee9ba99e9ca7f4
Goldyn19 <nwaezegolden2002@gmail.com>
2024-07-09 12:56:52 +0100

    edited urls

--- members/urls.py
+++ members/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import SignUpView, LoginView
 
 urlpatterns = [
-    path('register/', SignUpView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('register', SignUpView.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
 
 ]
