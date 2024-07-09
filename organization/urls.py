696eaf733133c26dfcf93dcb7ea9cb60c416d9dc
Goldyn19 <nwaezegolden2002@gmail.com>
2024-07-09 12:58:39 +0100

    edited all urls

--- organization/urls.py
+++ organization/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import OrganisationListCreateView, OrganisationDetailView,  AddUserToOrganisationView
 from members.views import UserDetailView
 urlpatterns = [
-    path('organisations/', OrganisationListCreateView.as_view(), name='organisation_list'),
-    path('organisations/<str:orgId>/', OrganisationDetailView.as_view(), name='organisation_detail'),
-    path('organisations/<str:orgId>/users/', AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
-    path('users/<str:id>/', UserDetailView.as_view(), name='user_detail'),
+    path('organisations', OrganisationListCreateView.as_view(), name='organisation_list'),
+    path('organisations/<str:orgId>', OrganisationDetailView.as_view(), name='organisation_detail'),
+    path('organisations/<str:orgId>/users', AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
+    path('users/<str:id>', UserDetailView.as_view(), name='user_detail'),
 ]
